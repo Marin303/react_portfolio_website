7f2b0479a95b6cf6d953be8bccd9c52b43fb84ed
Marin <marin.muktic3@gmail.com>
2023-06-03 15:23:36 +0200

    btn content, nav flex

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -4,9 +4,35 @@ const NavBar = () => {
   return (
     <div className="wrapper d-flex flex-column align-items-start justify-content-center">
       <nav className="bg-dark bg-gradient">
-        <ul>
-          <li className="li-navBar">{/* <a href="#"></a> */}</li>
+        <ul className="ulNav d-flex justify-content-center">
+          <li className="li-navBar mt-1">
+            \*<a href="#NameText">MARIN DEV</a>*/
+          </li>
         </ul>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-3 h-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="btn-content">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </nav>
       <section className="nameText text-white align-items-center justify-content-center">
         <h1>Marin Muktić</h1>
