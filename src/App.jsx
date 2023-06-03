ee708b4262d64597984bedd516d9ac2919ff2851
Marin <marin.muktic3@gmail.com>
2023-06-03 11:38:47 +0200

    bootstrap involved, background img, new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import NavBar from "./Pages/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 function App() {
   return (
     <div className="App">
       <NavBar>
-        
       </NavBar>
     </div>
   );
--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="containerNav">
-     <nav className="bg-dark bg-gradient">
-      <ul>
-        <li className="li-navBar"><a href="#"></a></li>
-      </ul>
-     </nav>
+    <div className="wrapper d-flex flex-column align-items-start justify-content-center">
+      <nav className="bg-dark bg-gradient">
+        <ul>
+          <li className="li-navBar">{/* <a href="#"></a> */}</li>
+        </ul>
+      </nav>
+      <section className="nameText text-white align-items-center justify-content-center">
+        <h1>Marin Muktić</h1>
+        <h2>Frontend Developer</h2>
+      </section>
     </div>
   );
 };
