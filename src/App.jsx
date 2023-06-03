312f391016a9927318f813a62932644ca5b181e2
Marin <marin.muktic3@gmail.com>
2023-06-03 18:57:34 +0200

    new backgrounds, custom scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FirstPage from "./Pages/FirstPage";
+import AboutMe from "./Pages/AboutMe";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
       <FirstPage />
+      <AboutMe/>
     </div>
   );
 }
