ff1ca1dd4d7462ebe994b877fd8d78b325faabdf
Marin <marin.muktic3@gmail.com>
2023-06-04 13:56:52 +0200

    cover, profile images, devicons, new keyframe

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import programmingImage from "../Images/programming.jpg";
+import imageOfMe from "../Images/me-img.png";
 
 const AboutMe = () => {
   return (
-    <div className='about-container'>AboutMe</div>
-  )
-}
+    <div className="about-container text-center align-middle">
+      <h2 className="text-white pt-5 d-inline-block custom-border">About Me</h2>
+      <div className="about-wrapper">
+        <img
+          src={programmingImage}
+          alt="programming"
+          className="rounded-top coverImage"
+          style={{ objectFit: "cover", width: "100%", maxHeight: "150px" }}
+        />
+        <div className="image-wrapper">
+          <img
+            src={imageOfMe}
+            alt="me"
+            className="imageOfMe rounded-circle"
+            style={{
+              objectFit: "cover",
+              maxWidth: "150px",
+              maxHeight: "150px",
+            }}
+          />
+        </div>
+        <div className="textAbout">
+          <h3>Marin Muktić</h3>
+          <h4>Frontend Developer</h4>
+          <p>Passionate about technology</p>
+          <p>Constantly seeking new opportunities to enhance my experience.</p>
+          <div className="d-flex flex-wrap align-items-center">
+            <p className="mb-0">Skills:</p>
+            <i className="devicon-html5-plain mx-1">HTML</i>
+            <i className="devicon-css3-plain mx-1">CSS</i>
+            <i className="devicon-javascript-plain mx-1"> JS</i>
+            <i className="devicon-react-original mx-1">REACT</i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
