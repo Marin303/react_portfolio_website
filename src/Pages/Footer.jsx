cc43e4b831c82d8fb6e34699f8d79a8752c9b783
Marin <marin.muktic3@gmail.com>
2023-06-04 19:12:01 +0200

    form nested, hover, active

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -10,11 +10,23 @@ const Footer = () => {
       </div>
       <div className="d-flex justify-content-center flex-wrap">
         <fieldset className="contact-container">
-          <legend>Contact information</legend>
-          <fieldset>
-            <legend>Contact</legend>
-          </fieldset>
+          <legend>Contact via Email</legend>
+          <form action="mailto:mmuktic3@gmail.com" method="get">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required/>
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="example@email.com" required/>
+
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" id="subject" name="subject" required/>
+
+            <label htmlFor="text">Text:</label>
+            <textarea name="text" id="text" cols="30" rows="10" required></textarea>
+            <button className="d-block send" type="submit">Send</button>
+          </form>
         </fieldset>
+
         <fieldset className="contact-container">
           <legend>Contact information</legend>
           <fieldset>
