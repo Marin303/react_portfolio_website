07c3dea992edf395ad9f94d5d4ffc4914ec85193
Marin <marin.muktic3@gmail.com>
2023-06-18 16:59:19 +0200

    new projects added

--- src/Pages/FeaturedProjects.jsx
+++ src/Pages/FeaturedProjects.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import gifChat from "../Images/chatapp2.gif";
+import passGenImg from "../Images/password-generator.png";
+import ecommerceImg from "../Images/ecommerce-desktop.png";
+import wheaterImg from "../Images/wheater_forecast_desktop.png";
 const FeaturedProjects = () => {
   return (
     <div className="featuredContainer" id="projects">
@@ -7,7 +10,7 @@ const FeaturedProjects = () => {
         Featured Projects
       </h2>
       <div className="content">
-        <section className="projects">
+        <section className="projects pb-5">
           <img
             className="rounded-top-3"
             src={gifChat}
@@ -16,25 +19,116 @@ const FeaturedProjects = () => {
           />
           <h2 className="text-white">Web Chat App</h2>
           <p className="text-white">Chat created with React</p>
-          <div className="link-project rounded-3">
-            <a
-              href="https://github.com/Marin303/react_webchat_scaledrone"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub - repository
-            </a>
+          <div className="d-flex">
+            <div className="link-project rounded-3">
+              <a
+                href="https://github.com/Marin303/react_webchat_scaledrone"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub - repository
+              </a>
+            </div>
+            <div className="link-project rounded-3">
+              <a
+                href="https://chat-app-marin.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Project - deploy
+              </a>
+            </div>
           </div>
         </section>
         <section className="projects pb-5">
           <img
             className="rounded-top-3"
-            src="https://dummyimage.com/300x300/777777/ffffff.png"
-            alt="300x300"
-            style={{ width: "300px", maxHeight: "250px" }}
+            src={passGenImg}
+            alt="password-generator"
+            style={{ width: "300px", height: "250px" }}
           />
-          <h2 className="text-white">Placeholder</h2>
-          <p className="text-white">Second Project</p>
+          <h2 className="text-white">Password Generator</h2>
+          <p className="text-white">Password generator tool</p>
+          <div className="d-flex">
+            <div className="link-project rounded-3">
+              <a
+                href="https://github.com/Marin303/react_password_generator"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub - repository
+              </a>
+            </div>
+            <div className="link-project rounded-3">
+              <a
+                href="https://password-generator-marin.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Project - deploy
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="projects pb-5">
+          <img
+            className="rounded-top-3"
+            src={ecommerceImg}
+            alt="password-generator"
+            style={{ width: "300px", height: "250px" }}
+          />
+          <h2 className="text-white">Admin dashboard</h2>
+          <p className="text-white">Ecommerce admin dashboard</p>
+          <div className="d-flex">
+            <div className="link-project rounded-3">
+              <a
+                href="https://github.com/Marin303/ecommerce"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub - repository
+              </a>
+            </div>
+            <div className="link-project rounded-3">
+              <a
+                href="https://github.com/Marin303/ecommerce"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Project - in progress
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="projects pb-5">
+          <img
+            className="rounded-top-3"
+            src={wheaterImg}
+            alt="wheater-app"
+            style={{ width: "300px", height: "250px" }}
+          />
+          <h2 className="text-white">Wheater forecast</h2>
+          <p className="text-white">Real-time weather forecast app</p>
+          <div className="d-flex">
+            <div className="link-project rounded-3">
+              <a
+                href="https://github.com/Marin303/wheater_forecast_react"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub - repository
+              </a>
+            </div>
+            <div className="link-project rounded-3">
+              <a
+                href="https://weather-app-marin.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Project - deploy
+              </a>
+            </div>
+          </div>
         </section>
       </div>
     </div>
