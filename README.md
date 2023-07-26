5ab05c02be0008025bfed758107311a4b803fb7b
Marin <marin.muktic3@gmail.com>
2023-07-26 21:51:25 +0200

    featured projects with react slick carousel

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ npm i bootstrap@5.3.0
 ```
 npm install gsap
 ```
+```
+npm install react-slick slick-carousel
+```
 [Deploy preview here](marin-portfolio-website.netlify.app)
--- src/Pages/FeaturedProjects.jsx
+++ src/Pages/FeaturedProjects.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import WebChat from "../Projects/WebChat";
 import PasswordGen from "../Projects/PasswordGen";
 import Ecommerce from "../Projects/Ecommerce";
@@ -6,21 +10,33 @@ import WeatherForecast from "../Projects/WeatherForecast";
 import IpadressTracker from "../Projects/IpadressTracker";
 import CountrySearch from "../Projects/CountrySearch";
 import LinkSharingApp from "../Projects/LinkSharingApp";
+/* import ArrowDownIcon from "../Icons/ArrowDownIcon"; */
 
 const FeaturedProjects = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  
+
   return (
-    <div className="featuredContainer" id="projects">
+    <div className="featuredContainer bg-dark" id="projects">
       <h2 className="text-white d-inline-block custom-border mt-3">
         Featured Projects
       </h2>
-      <div className="content">
-        <WebChat/>
-        <PasswordGen/>
-        <Ecommerce/>
-        <WeatherForecast />
-        <IpadressTracker/>
-        <CountrySearch/>
-       <LinkSharingApp/>
+      <div className="content mt-5">
+        <Slider {...settings}>
+          <WebChat/>
+          <PasswordGen/>
+          <Ecommerce/>
+          <WeatherForecast/>
+          <IpadressTracker/>
+          <CountrySearch/>
+          <LinkSharingApp/>
+        </Slider>
       </div>
     </div>
   );
