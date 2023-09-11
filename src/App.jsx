0a9354b8d1d6d33b08d4804e2dd8c1e522438a73
Zlatko Furčić <zlatko.furcic@gmail.com>
2023-09-11 11:37:23 +0200

    layout improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import "./App.scss";
 import FirstPage from "./Pages/FirstPage";
+import NavigationMenu from "./Shared/NavigationMenu"
 import AboutMe from "./Pages/AboutMe";
 import FeaturedProjects from "./Pages/FeaturedProjects";
 import Footer from "./Pages/Footer";
 
-const App = () => {
+const App = ({ isVisible, btnContentRef }) => {
   return (
     <div className="app">
-      <div className="left">
-        <FirstPage />
-      </div>
-      <div className="rigth">
-        {/* <AboutMe />
+      <header>
+        {/* <NavigationMenu isVisible={isVisible} /> */}
+      </header>
+      <div className="content">
+        <div className="left">
+          <FirstPage />
+        </div>
+        <div className="rigth">
+          test
+          {/* <AboutMe />
         <FeaturedProjects />
-        <Footer /> */}
+         */}
+        </div>
+
       </div>
-
-
+      {/* <Footer /> */}
     </div>
   );
 };
